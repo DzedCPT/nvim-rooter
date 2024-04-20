177599428d55717de473c3dcaf85ab2e1e9e10fa
DzedCPT <jedda@plumerai.com>
2024-04-20 15:59:30 +0200

    Add config

--- lua/nvim-rooter.lua
+++ lua/nvim-rooter.lua
@@ -1,6 +1,14 @@
 local M = {}
 
-local NvimRooter= {}
+M.config = {
+	-- TODO: Need to implement more config fields
+	-- rooter_cd_cmd = 'cd'
+	-- rooter_silent_chdir = false,
+	-- rooter_resolve_links = false
+	-- rooter_manual_only=false
+	-- rooter_change_directory_for_non_project_files=''
+	rooter_patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
+}
 
 M.setup = function(config)
 end
