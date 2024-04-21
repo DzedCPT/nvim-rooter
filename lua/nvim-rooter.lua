6f215a7eb343ccece4835979fd9fe2570d8f933f
DzedCPT <jedda@plumerai.com>
2024-04-21 13:47:09 +0200

    Basic rooting works

--- lua/nvim-rooter.lua
+++ lua/nvim-rooter.lua
@@ -10,17 +10,65 @@ M.config = {
 	rooter_patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
 }
 
-M.setup = function(config)
+M.setup = function(config) end
+
+function current_dir()
+	return vim.fn.expand("%:p:h") -- Gets the current file's directory
 end
+
+function has(dir, filename)
+	-- ZZZ: Can probably make this matching more complex using glob patterns.
+	return vim.fn.glob(dir .. "/" .. filename) ~= ""
+end
+
+function is_root(dir)
+	for _, root_pattern in ipairs(M.config.rooter_patterns) do
+		if has(dir, root_pattern) then
+			return true
+		end
+	end
+	return false
+end
+
+function parent_dir(dir)
+	return vim.fn.fnamemodify(dir, ":h")
+end
+
+function change_workdir(dir)
+	if dir == vim.fn.getcwd() then
+		return
+	end
+	vim.api.nvim_set_current_dir(dir)
+end
+
 local function root()
-	print("root")
+	local current_dir = current_dir()
+
+	root_found = false
+	while not root_found do
+		if is_root(current_dir) then
+			root_found = true
+		elseif current_dir == '/' then
+			break
+		else
+			current_dir = parent_dir(current_dir)
+			-- What if root is never found? How do we get out of this infinite loop?
+		end
+	end
+
+	if root_found then
+		change_workdir(current_dir)
+	end
 end
 
 local group = vim.api.nvim_create_augroup("NvimRooter", { clear = true })
 vim.api.nvim_create_autocmd({ "VimEnter", "BufReadPost", "BufEnter" }, {
-	group=group,
+	group = group,
 	pattern = "*", -- Applies to all files
 	callback = root,
 })
+
+-- For debugging:
+-- root()
 
 return M
